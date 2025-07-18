4c2359276763417bf1c63ed6e327c3d83cc10688
TechAoba <TechAoba@163.com>
2025-07-18 15:11:56 +0800

    add new recovery state

---
+++ scenes/characters/player_state_recovering.gd
@@ -0,0 +1,15 @@
+extends PlayerState
+class_name PlayerStateRecovering
+
+const DURATION_RECOVERING := 500
+
+var time_start_recovery := Time.get_ticks_msec()
+
+func _enter_tree() -> void:
+	player.velocity = Vector2.ZERO
+	animation_player.play("recover")
+
+
+func _process(_delta: float) -> void:
+	if Time.get_ticks_msec() - time_start_recovery > DURATION_RECOVERING:
+		state_transition_requested.emit(Player.State.MOVING)
