076273f4e2ec62dbfae3e388b91be389f1e24a7c
TechAoba <TechAoba@163.com>
2025-07-25 15:31:32 +0800

    add shadow and control indicator

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -4,12 +4,19 @@ class_name Player
 enum ControlScheme { CPU, P1, P2 }
 enum State { MOVING, TACKLING, RECOVERING, PREP_SHOOT, SHOOTING, PASSING }
 
+const CONTROL_SCHEME_MAP : Dictionary = {
+	ControlScheme.CPU : preload("res://assets/art/props/cpu.png"),
+	ControlScheme.P1 : preload("res://assets/art/props/1p.png"),
+	ControlScheme.P2 : preload("res://assets/art/props/2p.png"),
+}
+
 @export var ball: Ball
 @export var control_scheme: ControlScheme
 @export var speed: float = 80.0
 @export var power: float = 70.0
 
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
+@onready var control_sprite: Sprite2D = %ControlSprite
 @onready var player_sprite: Sprite2D = %PlayerSprite
 @onready var teammate_detection_area: Area2D = %TeammateDetectionArea
 
@@ -18,11 +25,13 @@ var heading := Vector2.RIGHT
 var state_factory := PlayerStateFactory.new()
 
 func _ready() -> void:
+	set_control_texture()
 	switch_state(State.MOVING)
 	
 
 func _process(_delta: float) -> void:
 	flip_sprite()
+	set_sprite_visibility()
 	# 让玩家移动起来，直接使用move_and_slide()即可
 	move_and_slide()
 
@@ -59,8 +68,17 @@ func flip_sprite() -> void:
 		player_sprite.flip_h = true
 
 
+func set_sprite_visibility() -> void:
+	# 持球的CPU或者真人玩家会显示control标志
+	control_sprite.visible = has_ball() or not control_scheme == ControlScheme.CPU
+
+
 func has_ball() -> bool:
 	return ball.carrier == self
+
+
+func set_control_texture() -> void:
+	control_sprite.texture = CONTROL_SCHEME_MAP[control_scheme]
 
 
 func on_animation_complete() -> void:
